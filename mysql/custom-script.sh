dc5a54c1fb44475a98dcdf03836bd2bff27c04b5
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-11 21:50:37 +0530

    project

--- mysql/custom-script.sh
+++ mysql/custom-script.sh
@@ -2,3 +2,8 @@
 
 if [ -f /tmp/mysql-root-password.txt ]; then
     PASSWORD=$(cat /tmp/mysql-root-password.txt)
+    echo "Accessed MySQL root password"
+else
+    echo "MySQL Root password file not found"
+    exit 1
+fi
